f916ce1e793563c382c345d881e17bad29b5ccb1
Moa <m.o.a.tyskland@gmail.com>
2023-04-30 12:38:49 +0200

    Add project files.

---
+++ Listen in Klassen/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listen_in_Klassen
+{
+    
+    public partial class Form1 : Form
+    {
+        //Attribute
+    
+        StreamReader myReader = new StreamReader("user.csv");
+        List<Person> PersonenListe = new List<Person> ();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            while (!myReader.EndOfStream)
+            {
+                string eineZeile = myReader.ReadLine();
+                string[] UserArayEineZeile = eineZeile.Split(',');
+                PersonenListe.Add(new Person(Int32.Parse(UserArayEineZeile[0]), UserArayEineZeile[1], UserArayEineZeile[2]));
+            }
+
+            foreach (var item in PersonenListe)
+            {
+                listBox1.Items.Add(item.name);
+            }
+            AllePersonenAusgeben();
+        }
+
+
+        private void AllePersonenAusgeben()
+        {
+            foreach (Person person in PersonenListe)
+            {
+                person.datenAusgeben(textBox1);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            foreach (Person person in PersonenListe)
+            {
+                if (listBox1.SelectedItem.ToString() == person.name)
+                {
+                    person.datenAusgeben(textBox1);
+                }
+            }
+
+        }
+    }
+}
---
+++ Listen in Klassen/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listen_in_Klassen
+{
+    class Person
+    {
+
+        //Atribute
+        public int id;
+        public string name;
+        public string telefonnummer;
+        private string passwort = "geheimpasswort";
+
+
+        //Konstruktor
+        public Person(int id, string name, string telefonnummer)
+        {
+            this.id = id;
+            this.name = name;
+            this.telefonnummer = telefonnummer;
+
+        }
+
+        //getter setter
+        public bool setPasswort(string passwort)
+        {
+            if (passwort.Length >= 6)
+            {
+                this.passwort = passwort;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public string getPasswort()
+        {
+            return this.passwort;
+        }
+        //Methoden
+        public void datenAusgeben(TextBox txtBox)
+        {
+            txtBox.Text += "ID: " + this.id + "\r\nName: " + this.name + "\r\nTel. : " + this.telefonnummer + "\r\n";
+            txtBox.Text += "\r\nPasswort: " + this.passwort + "\r\n";
+
+        }
+
+    }
+}
